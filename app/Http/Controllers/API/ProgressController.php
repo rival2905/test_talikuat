b31ca18ff3875e4c74c55de053653264c61f0bde
xart22 <fajar.rn22@gmail.com>
2023-02-13 13:36:03 +0700

    fixing api

--- app/Http/Controllers/API/ProgressController.php
+++ app/Http/Controllers/API/ProgressController.php
@@ -28,14 +28,17 @@ class ProgressController extends Controller
         }
         foreach ($data as $d) {
             $rencana = 0;
+            $realisasi = 0;
             $days = 0;
             $now = date('Y-m-d');
             $end_date = date('Y-m-d', strtotime($d->tgl_spmk . "+" . $d->detailWithJadual->lama_waktu . " days"));
             $now = new DateTime($now);
             $end = new DateTime($end_date);
             $interval = $end->diff($now);
-            $days = $interval->days;
-
+            $days = $d->detailWithJadual->lama_waktu - $interval->days;
+            if ($days < 0) {
+                $days = 0;
+            }
             foreach ($d->detailWithJadual->jadualDetail as $jadual) {
                 foreach ($jadual->detail as $detail) {
                     if (strtotime($detail->tanggal) <= strtotime(date('Y-m-d'))) {
@@ -43,23 +46,29 @@ class ProgressController extends Controller
                     }
                 }
             }
-            $d->detailWithJadual->jadualDetail = $rencana;
+            foreach ($d->laporanUptdAproved as $laporan) {
+                $realisasi += floatval($laporan->realisasi);
+            }
+            $d["days"] = $days;
+            $d['realisasi'] =  number_format($realisasi, 2, '.', '.');
+            $d['rencana'] = number_format($rencana, 2, '.', '.');
+            $d['deviasi'] = number_format($rencana - $realisasi, 2, '.', '.');
         }
         return response()->json([
             'status' => 'success',
-            'data' => $data
+            'data' => $data[1]
         ]);
     }
 
     public function getDataPembangunan2022()
     {
-        $data =   DB::connection('talikuat22')->table('data_umum')->get();
+        $data = DB::connection('talikuat22')->table('data_umum')->get();
         foreach ($data as $key => $value) {
-            $data[$key]->laporan_konsultan =  DB::connection('talikuat22')->table('laporan_konsultan')->where('data_umum_id', $value->id)->orderBy('id', 'asc')->get();
-            $data[$key]->data_umum_detail = DB::connection('talikuat22')->table('data_umum_detail')->where([['id', $value->id], ['is_active', 1]])->first();
-            $data[$key]->data_umum_detail->jadual = DB::connection('talikuat22')->table('jadual')->where('data_umum_detail_id', $data[$key]->data_umum_detail->id)->get();
-            if (count($data[$key]->data_umum_detail->jadual) == 0) {
-                unset($data[$key]);
+            $value->laporan_konsultan =  DB::connection('talikuat22')->table('laporan_konsultan')->where('data_umum_id', $value->id)->orderBy('id', 'asc')->get();
+            $value->data_umum_detail = DB::connection('talikuat22')->table('data_umum_detail')->where([['id', $value->id], ['is_active', 1]])->first();
+            $value->data_umum_detail->jadual = DB::connection('talikuat22')->table('jadual')->where('data_umum_detail_id', $value->data_umum_detail->id)->get();
+            if (count($value->data_umum_detail->jadual) == 0) {
+                unset($value);
             }
         }
         foreach ($data as $item) {
@@ -73,6 +82,7 @@ class ProgressController extends Controller
                 unset($item);
             }
         }
+
 
         return response()->json([
             'status' => 'success',
