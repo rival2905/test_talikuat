f3a45d14dba0740aecf80f41eee37e6808b7c088
xart22 <fajar.rn22@gmail.com>
2023-03-21 16:59:13 +0700

    update pnajang v

--- resources/views/data-umum/edit.blade.php
+++ resources/views/data-umum/edit.blade.php
@@ -143,7 +143,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label>Panjang KM</label>
-                            <input type="number" step="0.01" name="panjang_km" value="{{ $data_umum->detail->panjang_km }}" id="panjang_km" class="form-control" required autocomplete="off" />
+                            <input type="number" step="0.001" name="panjang_km" value="{{ $data_umum->detail->panjang_km }}" id="panjang_km" class="form-control" required autocomplete="off" />
                         </div>
                     </div>
                     <div class="col-md-4">
