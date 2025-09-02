999968a27577fd2dfd583bd01f74d764d4ea1b27
fajar.rn22@gmail.com <fajar.rn22@gmail.com>
2025-09-02 16:02:32 +0700

    new modal

--- resources/views/data-umum/index.blade.php
+++ resources/views/data-umum/index.blade.php
@@ -35,8 +35,15 @@
                             <td>{{$data->detail->konsultan->name}}</td>
                             <td>{{$data->detail->ppk->nama ?? ''}}</td>
                             <td>
-                                <div class="flex space-x-1 space-y-2 justify-center">
+                        <a class="btn btn-mat btn-warning mb-3" data-bs-toggle="modal" data-bs-target="#modalAction"
+                                data-id="{{ $data->id }}"
+                                onclick="handleAction(this)"
+                        >
+                            Proses
+                        </a>
+                                {{-- <div class="flex space-x-1 space-y-2 justify-center">
                                     <a href="{{route('data-umum.show',$data->id) }}"
+                                        data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Detail"
                                         class="btn btn-mat btn-success waves-effect waves-light"><i
                                             class="bx bx-search-alt-2"></i></a>
                                     @if (Auth::user()->userDetail->role != 7 && date('Y') == $data->thn)
@@ -57,7 +64,27 @@
                                     <a href="{{route('admin.data-umum.document-category.show',$data->id) }}"
                                         class="btn btn-mat btn-sm btn-success waves-effect waves-light">KKK</a>
                                     @endif
-                                </div>
+                                </div> --}}
+
+                                {{-- <div class="flex space-x-1 space-y-2 justify-center">
+                                    <a href="{{route('data-umum.show',$data->id) }}"
+                                        class="btn btn-mat btn-success waves-effect waves-light fs-6">Detail</a>
+                                    @if (Auth::user()->userDetail->role != 7 && date('Y') == $data->thn)
+                                    <a href="{{ route('data-umum.edit', $data->id) }}"
+                                        class="btn btn-mat btn-warning waves-effect waves-light fs-6">Edit</a>
+                                    @endif
+                                    <a href="{{route('upload.dataumum',$data->id)}}"
+                                        class="btn btn-mat btn-primary waves-effect waves-light fs-6">Dokumen Kontrak</a>
+                                    @if (Auth::user()->userDetail->role == 1 || Auth::user()->userDetail->role == 2 || Auth::user()->userDetail->role ==5)
+                                    <a href="{{ route('penilaian-penyedia.index', $data->id) }}"
+                                        class="btn btn-mat btn-warning waves-effect waves-light mt-1 fs-6">Penilaian Penyedia
+                                    </a>
+                                    @endif
+                                    @if (Auth::user()->userDetail->role == 1 || Auth::user()->userDetail->role == 2 || Auth::user()->userDetail->role ==5)
+                                    <a href="{{route('admin.data-umum.document-category.show',$data->id) }}"
+                                        class="btn btn-mat btn-sm btn-success waves-effect waves-light fs-6">Kendali Kontrak</a>
+                                    @endif
+                                </div> --}}
 
                             </td>
                         </tr>
@@ -66,6 +93,45 @@
                 </table>
             </div>
         </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="modalAction" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="modalActionLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="modalActionLabel">Pilih</h1>
+                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body d-flex justify-content-center align-items-center">
+        
+                <div class="d-flex  justify-center flex-wrap gap-1 ">
+                                       <a href="{{route('data-umum.show',0) }}"
+                                        class="btn btn-mat btn-success waves-effect waves-light fs-6" >Detail</a>
+                                    @if (Auth::user()->userDetail->role != 7 && date('Y') == $data->thn)
+                                    <a href="{{ route('data-umum.edit', 0) }}"
+                                        class="btn btn-mat btn-warning waves-effect waves-light ">Edit</a>
+                                    @endif
+                                    <a href="{{route('upload.dataumum',0)}}"
+                                        class="btn btn-mat btn-primary waves-effect waves-light ">Dokumen Kontrak</a>
+                                    @if (Auth::user()->userDetail->role == 1 || Auth::user()->userDetail->role == 2 || Auth::user()->userDetail->role ==5)
+                                    <a href="{{ route('penilaian-penyedia.index', 0) }}"
+                                        class="btn btn-mat btn-danger waves-effect waves-light ">Penilaian Penyedia
+                                    </a>
+                                    @endif
+                                    @if (Auth::user()->userDetail->role == 1 || Auth::user()->userDetail->role == 2 || Auth::user()->userDetail->role ==5)
+                                    <a href="{{route('admin.data-umum.document-category.show',0) }}"
+                                        class="btn btn-mat  btn-info waves-effect waves-light ">Kendali Kontrak</a>
+                                    @endif
+                                </div>
+            </div>
+            
+        </div>
+
     </div>
 </div>
 <div class="modal fade" id="dataThn" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
@@ -114,5 +180,25 @@
             }
         });
     });
+
+    function handleAction(event) {
+        console.log(event);
+        const id = event.getAttribute('data-id');
+        console.log(id);
+        const urlShow = `{{ route('data-umum.show',1) }}`.replace('1', id)
+        const urlEdit = `{{ route('data-umum.edit',1) }}`.replace('1', id)
+        const urlUpload = `{{ route('upload.dataumum',1) }}`.replace('1', id)
+        const urlPenilaian = `{{ route('penilaian-penyedia.index',1) }}`.replace('1', id)
+        const urlKendali = `{{ route('admin.data-umum.document-category.show',1) }}`.replace('1', id)
+
+        const modal = document.getElementById('modalAction');
+        const links = modal.querySelectorAll('a');
+
+        links[0].setAttribute('href', urlShow);
+        links[1].setAttribute('href', urlEdit);
+        links[2].setAttribute('href', urlUpload);
+        links[3].setAttribute('href', urlPenilaian);
+        links[4].setAttribute('href', urlKendali);
+    }
 </script>
 @endsection
